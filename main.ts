9bd714dce11e451442e364a0f7e590c0db601b51
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-26 13:10:24 -0500

    First commit

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jet Lu
+ * Created on: Feb 2026
+ * This program will turn a light on and off.
 */
 
-basic.showString('Hello, World!')
+// startup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P16, 0) // this turns the pin off
+
+// turns light on
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    pause(500)
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turns light off
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    pause(500)
+    basic.showIcon(IconNames. No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
