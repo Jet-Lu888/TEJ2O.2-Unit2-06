7b149da79c588903f7312661989c59ce832d762e
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-26 22:26:33 +0000

    Created the program in python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,24 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Jet Lu
+Created on: Feb 2026
+This module is a Micro:bit MicroPython program that can turn an LED on and off.
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+
+# startup
+display.clear()
+display.show(Image.HAPPY)
+pin16.write_digital(0)
+sleep(500)
+
+while True:
+    # turn light on
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.YES)
+    # turn light off
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.NO)
